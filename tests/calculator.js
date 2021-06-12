de08ba6909d214e08727e8cd2b7ef084c57317d9
Autobot1k <ur0bor0s@protonmail.com>
2021-06-12 13:42:57 -0500

    update

--- tests/calculator.js
+++ tests/calculator.js
@@ -23,26 +23,4 @@ describe('demo calculator tests', function(){
         browser.sleep(4000)
     });
 
-    it('add test', function() {
-
-        //browser.get('http://juliemr.github.io/protractor-demo/');
-        homepage.goToURL('http://juliemr.github.io/protractor-demo/');
-
-        //element(by.model('first')).sendKeys('2');
-        homepage.enterFirstNumber('2');
-
-        //element(by.model('second')).sendKeys('2');
-        homepage.enterSecondNumber('2');
-
-        //element(by.css('[ng-click="doAddition()"]')).click();
-        homepage.clickGo1();
-
-        // var result = element(by.cssContainingText('.ng-binding', '4'))
-        // expect(result.getText()).toEqual('4');
-        homepage.verifyResult('4');
-
-        browser.sleep(3000)
-    });
-
-
 })
